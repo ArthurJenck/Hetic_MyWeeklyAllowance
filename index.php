e24be5418e4cc6157eedd8f09df193d16ae617d1
Arthur Jenck <arthurjenckdev@gmail.com>
2025-11-26 15:09:40 +0100

    added error handling on index

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+// Afficher toutes les erreurs pour le débogage
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Relais vers le dossier public
 require_once __DIR__ . '/public/index.php';
